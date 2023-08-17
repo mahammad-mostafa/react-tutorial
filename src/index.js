1416785db95e259440e682f0f34355b38a9f7884
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-08-17 07:57:24 +0300

    Removed BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>);
+root.render(<React.StrictMode><App /></React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
